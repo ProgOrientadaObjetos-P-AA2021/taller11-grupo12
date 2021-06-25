4ed2d9e39fe686d3fc3383973d1ca271955a88d4
JamilErasmo <jamilerasmoq@gmail.com>
2021-06-25 10:45:32 -0500

    taller11

--- taller11/src/taller11/MenudeNiños.java
+++ taller11/src/taller11/MenudeNiños.java
@@ -10,5 +10,37 @@ package taller11;
  * @author jamil
  */
 public class MenudeNiños {
-    
+    private double valorHelado;
+    private double valorPastel;
+
+    public MenudeNiños(String nP, double vM, double vI, double vH, double vP) {
+        super(nP, vM, vI);
+        valorHelado = vH;
+        valorPastel = vP;
+    }
+
+    public double obtenerValorHelado() {
+        return valorHelado;
+    }
+
+    public void establecerValorHelado(double vH) {
+        valorHelado = vH;
+    }
+
+    public double obtenerValorPastel() {
+        return valorPastel;
+    }
+
+    public void establecerValorPastel(double vP) {
+        valorPastel = vP;
+    }
+
+    public String toString() {
+        return String.format("Nombre del plato: %s\nValor del menu: %.2f\n"
+                + "Valor inicial del menu: %.2f\n"
+                + "valor de porcion de helado: %.2f\n"
+                + "Valor de porcion de pastel: %.2f\n",
+                 obtenerNombrePlato(), obtenerValorMenu(),
+                obtenerValorInicial(), obtenerValorHelado(), obtenerValorPastel());
+    }
 }
