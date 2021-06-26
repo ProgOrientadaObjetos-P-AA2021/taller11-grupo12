55a42cc5682365241d004b7281811bd3ca111f7c
JamilErasmo <jamilerasmoq@gmail.com>
2021-06-25 20:28:09 -0500

    taller11

--- taller11/src/taller11/MenuEconomico.java
+++ taller11/src/taller11/MenuEconomico.java
@@ -10,5 +10,31 @@ package taller11;
  * @author jamil
  */
 public class MenuEconomico {
-    
+    public class EconomicalMenu extends Menu{
+    private final double porcentajeDescuento = 0.10;
+
+    public EconomicalMenu(String nombrePlato, double valorInicial) {
+        super(nombrePlato, valorInicial);
+        setValorMenu();
+    }
+
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+
+    @Override
+    void setValorMenu() {
+        this.valorMenu = valorInicial - (valorInicial * porcentajeDescuento);
+    }
+    @Override
+    public String toString() {
+        this.nombrePlato += " + guarniciÃ³n + pastel";
+        System.out.println("-----------------------");
+        return String.format("%s   Porcentaje Descuento: %.2f\n", super.toString(), porcentajeDescuento);
+    }
+}
 }
